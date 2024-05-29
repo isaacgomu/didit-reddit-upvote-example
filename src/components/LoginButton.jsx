8cfc08699582ab95a71b44c85912cc8f96a5d0fb
Isaac Dalglish <163774292+isaacgomu@users.noreply.github.com>
2024-05-29 12:17:28 +0100

    Update LoginButton.jsx

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { signIn } from "next-auth/react"
  
-export function SignIn() {
+export function LoginButton() {
   return <Button onClick={() => signIn()}>Sign In</Button>
 }
